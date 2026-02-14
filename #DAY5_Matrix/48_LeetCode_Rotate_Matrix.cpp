e19b5baac3d2f5a3a76b06119e9065b7bd6b4732
AhirSonoo28 <ahirsonoo228@gmail.com>
2026-02-14 23:56:40 +0530

    day5of100

---
+++ #DAY5_Matrix/48_LeetCode_Rotate_Matrix.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                swap(matrix[i][j], matrix[i][n - 1 - j]);
+            }
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    sol.rotate(matrix);
+
+    cout << "Rotated Matrix:" << endl;
+    for (auto &row : matrix) {
+        for (auto &val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+//
